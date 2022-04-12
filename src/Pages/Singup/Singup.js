67248a9f780a66af81a5815feb62451e6aa86057
mehedy201 <mehedy643@gmail.com>
2022-04-12 15:48:47 +0600

    create all components

---
+++ src/Pages/Singup/Singup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Singup.css'
+
+const Singup = () => {
+    return (
+        <div>
+            <h2>this is singup</h2>
+        </div>
+    );
+};
+
+export default Singup;
