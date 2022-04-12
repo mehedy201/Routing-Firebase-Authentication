be78c83006d27f948847d1b87cbf62680c0ec322
mehedy201 <mehedy643@gmail.com>
2022-04-12 22:22:52 +0600

    added some style in navbar

--- src/Sheard/Header/Header.js
+++ src/Sheard/Header/Header.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './Header.css'
+import logo from '../../images/Mehedi_Hasan.png'
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="light">
         <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+            <img height={50} src={logo} alt="" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="mx-auto">
+            <Nav.Link className='fw-bold' href="#home">Home</Nav.Link>
+            <Nav.Link className='fw-bold' href="#shop">Shop</Nav.Link>
+            <Nav.Link className='fw-bold' href="#about">About</Nav.Link>
+            <Nav.Link className='fw-bold' href="#contact">Contact</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            <Nav.Link className='p-1' href="#deets"><button className='h_btn'>Log In</button> </Nav.Link>
+            <Nav.Link className='p-1' href="#deets"><button className='h_btn'>Sing Up</button> </Nav.Link>
           </Nav>
         </Navbar.Collapse>
         </Container>
