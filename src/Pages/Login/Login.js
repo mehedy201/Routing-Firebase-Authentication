26f9b5687bb756a51b1ab8dff9c0c001f85531fb
mehedy201 <mehedy643@gmail.com>
2022-04-14 17:11:52 +0600

    added requer auth component and work form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,24 +1,37 @@
 import { Button } from "bootstrap";
 import React, { useRef } from "react";
 import { Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import "./Login.css";
 
 const Login = () => {
   const emailRef = useRef('');
   const passwordRef = useRef('');
 
-  const navigat = useNavigate('')
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
 
-  const handleSubmitButton  = event =>{
+  const handleSubmitButtonLogIn  = event =>{
     event.preventDefault()
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    console.log(email, password);
+    signInWithEmailAndPassword(email, password);
   }
-
+  
+  
+  const navigat = useNavigate('')
   const navigatSingUp = event => {
     navigat('/singup')
+  }
+
+  if(user){
+    navigat('/home')
   }
 
   return (
@@ -34,7 +47,7 @@ const Login = () => {
             <Form.Label>Password</Form.Label>
             <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
           </Form.Group>
-          <Form.Control onClick={handleSubmitButton} type="submit" value="submit" />
+          <Form.Control onClick={handleSubmitButtonLogIn} type="submit" value="submit" />
         </Form>
         <p className='pt-3'>If don't have account? <span onClick={navigatSingUp} className="text-warning text-decoration-underline singup_link">Please SingUp</span></p>
       </div>
--- src/Pages/Singup/Singup.js
+++ src/Pages/Singup/Singup.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Singup.css'
+import auth from '../../firebase.init';
 
 const Singup = () => {
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+
+  // use navigate in login text
+  const navigate = useNavigate()
+  const navigatLogin = event => {
+    navigate('/login')
+  }
+  // get input value
+  const nameRef = useRef('');
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+
+  const handleSubmitButtonSingUp  = event =>{
+    event.preventDefault()
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    createUserWithEmailAndPassword(email, password);
+  }
+
+  if(user){
+    navigate('/home')
+  }
+
+
+
+
+
     return (
         <div className="login_area">
       <div className="w-50 inside_login">
@@ -10,18 +47,19 @@ const Singup = () => {
         <Form>
           <Form.Group className="mb-3" controlId="formGroupName">
             <Form.Label>Your Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter Your Name" />
+            <Form.Control ref={nameRef} type="text" placeholder="Enter Your Name" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control ref={passwordRef} type="password" placeholder="Password" />
           </Form.Group>
-          <Form.Control type="submit" value="submit" />
+          <Form.Control onClick={handleSubmitButtonSingUp} type="submit" value="submit" />
         </Form>
+        <p className='mt-3'>All ready have an Account <span onClick={navigatLogin} className='text-warning text-decoration-underline singup_btn'>Please Log In</span></p>
       </div>
     </div>
     );
